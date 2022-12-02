88717a5f7073c767dee836570ac7eba361054dea
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2022-12-02 10:35:38 +0100

    Initial Commit

---
+++ ArchScratch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArchScratch
+//
+//  Created by Dmitry Bespalov on 02.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
