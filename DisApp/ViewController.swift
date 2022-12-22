e35b0cb25ba5cdb7fe5d2db31b07006d0d4fca0c
Dmitry Bespalov <dima@safe.global>
2022-12-22 17:16:47 +0100

    Initial Commit

---
+++ DisApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DisApp
+//
+//  Created by Dmitry Bespalov on 22.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
